e2d711c257ecbf0f7543b83c317463a9bed869b3
sawyersieja <sawyersieja@protonmail.com>
2024-03-23 19:00:03 -0700

    Completed all assignment requirements.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,72 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    //I think I made a mistake building that weather program
+    //and then immediately creating the Clojure program after.
+    //I'm kind of burnt out.
+    //I was hoping Kotlin would get most of my effort.
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var name: String
+    name = "First"
+
+    var age: Int
+    age = 42
+
+    var weight: Double
+    weight = 200.00
+
+    var filledOut: Boolean = checkBox(name, age, weight)
+    println("Filled out: $filledOut")
+
+    for(i in 1..5) {
+        if(i == 1) {
+            println("\nExample")
+        }
+        else if(i == 2) {
+            println("\tof")
+        }
+        else if (i == 3) {
+            println("\t\ta")
+        }
+        else if (i == 4) {
+            println("\t\t\tfor")
+        }
+        else if (i == 5) {
+            println("\t\t\t\tloop.")
+        }
+        else {
+            println("This should not be printed on the screen.")
+        }
     }
+
+    var x = 10
+    while(x > 0) {
+        println("While countdown: $x")
+        x--
+    }
+
+    println(" ")
+
+    do {
+        println("Dowhile countup: $x")
+        x++
+    } while (x < 11)
+
+    println("")
+
+    repeat(5) {count ->
+        println("Testing repeat function ${count + 1}")
+    }
+
+}
+
+fun checkBox(name: String,age: Int, weight: Double): Boolean {
+    if((name.isNotEmpty()) && (age > 1) && (weight > 0.0)) {
+        val fullName: String = "$name Last"
+        println("Name: $fullName")
+        println("Age: $age")
+        println("Weight: $weight lbs")
+        return true;
+    }
+    return false;
 }
